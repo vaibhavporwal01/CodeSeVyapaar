34295629cd948a8fc2a3ee0b2dd419c61925fe67
Kanishka Tyagi <kanishkatyagi10@gmail.com>
2024-12-27 17:47:12 +0530

    restart

---
+++ code/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
